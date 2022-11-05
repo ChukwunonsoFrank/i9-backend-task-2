4f9329bf299365dc5ff2d44162b7eacfdac86969
ChukwunonsoFrank <franknonso114@gmail.com>
2022-11-05 18:25:15 +0100

    fix: change Enum type for operation_type parameter

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ function compute(operation, x, y) {
 
 function extractResultAndOperationFromGPT3Response(output) {
   const splitResponse = output.split('is')
-  const operationType = String(splitResponse[2]).trim()
+  const operationType = String(splitResponse[2]).trim().slice(0, -1)
   const pivotSplit = splitResponse[1].split('and')
   const result = parseFloat(String(pivotSplit[0]).trim())
   return {
@@ -54,18 +54,17 @@ async function processGPT3Prompt(question) {
 
 export default {
   show: async (request, response, next) => {
+    const operationsEnum = ['addition', 'subtraction', 'multiplication']
     const { operation_type, x, y } = request.body
 
-    if (typeof operation_type === 'object') {
-      const result = compute(operation_type.value, x, y)
+    if (operationsEnum.includes(operation_type)) {
+      const result = compute(operation_type, x, y)
       response.status(200).json({
         slackUsername: 'franknonso',
         result,
-        operation_type: operation_type.value,
+        operation_type: operation_type,
       })
-    }
-
-    if (typeof operation_type === 'string') {
+    } else {
       const { operationType, result } = await processGPT3Prompt(operation_type)
       response.status(200).json({
         slackUsername: 'franknonso',
